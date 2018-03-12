8668a657073c31a71fff2b1513e2789eb20da6ca
krlargo <krlargo@ucdavis.edu>
2018-03-11 20:34:54 -0700

    Add init() for local vars

--- MVVM/MVVM/Models/User.swift
+++ MVVM/MVVM/Models/User.swift
@@ -14,6 +14,13 @@ class User {
     var email: String!
     var password: String!
     
+    init(firstName: String!, lastName: String!, email: String!, password: String!) {
+        self.firstName = firstName;
+        self.lastName = lastName;
+        self.email = email;
+        self.password = password;
+    }
+    
     init(vm: RegistrationViewModel) {
         self.firstName = vm.firstName;
         self.lastName = vm.lastName;
