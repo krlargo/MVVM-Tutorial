ed4b32b7c9ece2e30220dd336a17bb569b868ef8
krlargo <krlargo@ucdavis.edu>
2018-03-11 19:36:08 -0700

    Add init(RegistrationViewModel)

--- MVVM/MVVM/Models/User.swift
+++ MVVM/MVVM/Models/User.swift
@@ -13,4 +13,11 @@ class User {
     var lastName: String!
     var email: String!
     var password: String!
+    
+    init(vm: RegistrationViewModel) {
+        self.firstName = vm.firstName;
+        self.lastName = vm.lastName;
+        self.email = vm.email;
+        self.password = vm.password;
+    }
 }
