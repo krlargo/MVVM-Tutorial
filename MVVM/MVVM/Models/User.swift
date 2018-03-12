6e12188877cf4a53be757ea5ce694d326fbcecf6
krlargo <krlargo@ucdavis.edu>
2018-03-11 17:16:34 -0700

    Setup User domain object

---
+++ MVVM/MVVM/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  MVVM
+//
+//  Created by Kevin Largo on 3/11/18.
+//  Copyright © 2018 Kevin Largo. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var firstName: String!
+    var lastName: String!
+    var email: String!
+    var password: String!
+}
