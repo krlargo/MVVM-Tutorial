41f8f4981c0f3fb2f30510743243cb03af3e9d37
krlargo <krlargo@ucdavis.edu>
2018-03-11 17:20:16 -0700

    Setup and wire RegistrationTVC

---
+++ MVVM/MVVM/View Models/RegistrationTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RegistrationTableViewController.swift
+//  MVVM
+//
+//  Created by Kevin Largo on 3/11/18.
+//  Copyright © 2018 Kevin Largo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegistrationTableViewController: UITableViewController {
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    
+}
